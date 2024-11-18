f76e465c3ee10de1be0a206b07a684ab12b83072
Brian CHOW <brianchow26@gmail.com>
2024-11-18 19:03:01 +0100

    feat: add instagram section

---
+++ src/components/Card/CardInstagram.tsx
@@ -0,0 +1,38 @@
+import { FaHeart } from "react-icons/fa6";
+import { FaComment } from "react-icons/fa";
+
+export interface CardInstagramProps {
+  href: string;
+  image: string;
+  description: string;
+  likes: string;
+  comments: string;
+}
+
+function CardInstagram({
+  href,
+  image,
+  description,
+  likes,
+  comments
+}: CardInstagramProps) {
+  return (
+    <a href={href} className="text-white no-underline" target="_blank">
+      <div className="flex flex-col w-[179px]">
+        <img src={image} height={179} width={179} />
+        <div className="truncate">{description}</div>
+        <div className="flex justify-around">
+          <div className="flex items-center">
+            <FaHeart /> {likes}
+          </div>
+          <div className="flex items-center">
+            <FaComment />
+            {comments}
+          </div>
+        </div>
+      </div>
+    </a>
+  );
+}
+
+export default CardInstagram;
